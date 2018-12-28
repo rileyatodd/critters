48a7de7c2018a0b0e3781c833135a71fb80eedd5
Riley Todd <rileyatodd@gmail.com>
2018-12-28 16:18:12 -0600

    related some of the properties of the critters to each other and move randomly created critters across the screen

--- src/critters/dynamic.clj
+++ src/critters/dynamic.clj
@@ -20,17 +20,72 @@
   (doseq [[x y] ps]
     (q/point x y)))
 
+(def length-u "" 40)
+(def length-v "" 10)
+(def leg-length-coef-u "" 3)
+(def leg-length-coef-v "" 0.5)
+(def speed-u "" 3)
+(def speed-v "" 0.5)
+(def period-u "" 1.5)
+(def period-v "" 0.12)
+(def num-legs-u "" 40)
+(def num-legs-v "" 3)
+(def foot-r-u "" 2.5)
+(def foot-r-v "" 0.03)
+(def width-coef-u "" 0.5)
+(def width-coef-v "" 0.1)
+(def wavelength-u "" (/ q/PI 16)); in radians?
+(def wavelength-v "" 0.01)
+
 (defn random-critter []
-  (let [length (gauss 40 10)]
+  (let [length (gauss length-u length-v)
+        leg-length-coef (gauss leg-length-coef-u leg-length-coef-v)
+        speed (max 0.1 (gauss (+ speed-u (* 0.067 (- length-u length))) speed-v))
+        period (gauss (+ period-u (* 0.4 (- speed-u speed))) period-v)
+        num-legs (max 24 (gauss (+ num-legs-u (* 0.8 (- length length-u))) num-legs-v))
+        foot-r (max 1.4 (gauss (+ foot-r-u (* 0.07 (- num-legs-u num-legs))
+                                           (* 0.1 (- length length-u))) foot-r-v))
+        wavelength (gauss (+ wavelength-u (* -0.004 (- num-legs num-legs-u))) wavelength-v)]
     {:cx (q/random (u/w)) 
      :cy (q/random (u/h)) 
      :l length 
-     :w (* length (gauss 0.5 0.1)) 
-     :num-legs (gauss 20 5)
-     :period (gauss 1 0.1)}))
+     :w (* length (gauss width-coef-u width-coef-v)) 
+     :num-legs num-legs
+     :foot-r foot-r
+     :period period
+     :wavelength wavelength
+     :leg-length-coef leg-length-coef
+     :motion-amp (gauss (* leg-length-coef length 0.08) 0.4)
+     :speed speed
+     :hue (rand 255)}))
+
+(defn random-point-on-side [i]
+  (cond 
+    (= i 0) [0 (rand (u/h))]
+    (= i 1) [(rand (u/w)) 0]
+    (= i 2) [(u/w) (rand (u/h))]
+    (= i 3) [(rand (u/w)) (u/h)]))
+
+(defn random-line-thu-sketch []
+  (mapcat #(random-point-on-side %) (take 2 (shuffle (range 4)))))
+
+(defn scale-line-about-midpoint [[x0 y0 x1 y1] scale]
+  (let [rise (- y1 y0) 
+        run (- x1 x0) 
+        mid-x (+ x0 (/ run 2))
+        mid-y (+ y0 (/ rise 2))
+        new-half-rise (* scale 0.5 rise)
+        new-half-run (* scale 0.5 run)]
+    [(- mid-x new-half-run) (- mid-y new-half-rise)
+     (+ mid-x new-half-run) (+ mid-y new-half-rise)]))
+
+(defn random-critter-on-line []
+  (let [line (scale-line-about-midpoint (random-line-thu-sketch) 2)]
+    (merge (random-critter) {:cx (nth line 0) :cy (nth line 1) :line line})))
 
 (defn initial-state []
-  {:critters (map (fn [_] (random-critter)) (range 10))})
+  {:critters (map (fn [_] (random-critter-on-line))
+                  (range 10))})
 
 (defn setup []
   (q/frame-rate 30)
@@ -49,31 +104,53 @@
     (do
       (swap! reset-state? (fn [_] false))
       (initial-state))
-    state))
+    {:critters (as-> (:critters state) cs
+         (filter #(and (u/between? (:cx %) (nth (:line %) 0) (nth (:line %) 2))
+                       (u/between? (:cy %) (nth (:line %) 1) (nth (:line %) 3))) cs)
+         (if (< (rand) 0.025) (conj cs (random-critter-on-line)) cs)
+         (map (fn [c]
+                (let [speed (:speed c)
+                      [x0 y0 x1 y1] (:line c)
+                      slope (/ (- y1 y0) (- x1 x0))
+                      angle (apply angle (:line c))
+                      dx (* speed (q/cos angle))
+                      dy (* speed (q/sin angle))]
+                  (merge c {:cx (+ dx (:cx c)) 
+                            :cy (+ dy (:cy c))}))) 
+              cs))}))
 
-(defn critter [{:keys [cx cy l w num-legs period]}]
+(defn critter [{:keys [l w num-legs period wavelength leg-length-coef motion-amp period foot-r]}]
   (let [rad-per-leg (/ q/TWO-PI num-legs)]
     (q/no-fill)
-    (q/ellipse cx cy l w)
-    (q/with-translation [cx cy]
-      (doseq [rad (range 0 q/TWO-PI rad-per-leg)]
-        (let [torso-x (* l 0.5 (q/cos rad))
-              torso-y (* w 0.5 (q/sin rad))
-              foot-r 10
-              leg-length-coef 3
-              motion-coef 0.5
-              length (+ leg-length-coef 
-                        (* motion-coef (q/sin (+ (* (q/millis) (/ q/TWO-PI (* 1000 period))) 
-                                                 (Math/abs (- rad q/PI))))))
-              foot-x (* length torso-x)
-              foot-y (* length torso-y)]
-          (q/line torso-x torso-y foot-x foot-y)
-          (q/ellipse foot-x foot-y foot-r foot-r))))))
+    (q/stroke-weight 2)
+    (q/ellipse 0 0 l w)
+    (q/stroke-weight 1)
+    (doseq [rad (range q/PI 0 (- rad-per-leg))]
+      (let [torso-x (* l 0.5 (q/cos rad))
+            torso-y (* w 0.5 (q/sin rad))
+            foot-d (* 2 foot-r)
+            motion-t (* (q/millis) (/ q/TWO-PI (* 1000 period)))
+            motion-coef (* motion-amp (q/sin (+ motion-t 
+                                                (Math/abs (- (/ rad wavelength) q/PI)))))
+            motion-dx 0 #_(* motion-coef (q/cos rad))
+            motion-dy (* motion-coef (q/sin rad))
+            length (+ leg-length-coef motion-coef)
+            ankle-x (+ motion-dx (* leg-length-coef torso-x))
+            ankle-y (+ motion-dy (* leg-length-coef torso-y))
+            foot-x (+ ankle-x (* (q/cos rad) foot-r))
+            foot-y (+ ankle-y (* (q/sin rad) foot-r))]
+        (q/line torso-x torso-y ankle-x ankle-y)
+        (q/ellipse foot-x foot-y foot-d foot-d)
+        (q/line torso-x (- torso-y) ankle-x (- ankle-y))
+        (q/ellipse foot-x (- foot-y) foot-d foot-d)))))
 
 (defn draw-state [state]
   (q/background 60)
   (q/stroke 255)
   (doseq [c (:critters state)]
-    (critter c)))
+    (q/with-translation [(:cx c) (:cy c)]
+      (q/with-rotation [(+ q/PI (apply angle (:line c)))]
+        (q/stroke (:hue c) 35 255)
+        (critter c)))))
   
   
